bd7f0bc2f41f57fc5628c032ec939011211a1ce5
Thalyta C Fischer <thalytacfischer@gmail.com>
2019-11-11 12:31:11 -0200

    Aula4

---
+++ Aula3/aula3.py
@@ -0,0 +1,15 @@
+numero1 =10
+numero2 =15
+print('='*30)
+
+resultado = numero1 + numero2
+print (resultado)
+resultado = numero1 - numero2
+print (resultado)
+resultado = numero1 * numero2
+print (resultado)
+resultado = numero1 / numero2
+print (resultado)
+resultado = 'Thalyta' + 'Fischer'
+print (resultado)
+print('='*30)
