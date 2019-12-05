2f2d034a59119b17ff373c66bcf725dce61ce487
Thalytacf <-l>
2019-12-05 11:48:23 -0300

    Resoluções Aula19

--- Aula19/ex1.py
+++ Aula19/ex1.py
@@ -13,10 +13,42 @@ cadastroHBSIS = ['nome',   ['Alex'   ,'Paulo'  ,'Pedro'  ,'Mateus' ,'Carlos' ,'J
 # nome  Alex telefone: 4799991
 # idade Carlos é 15 anos
 # email de Mateus é d@d.com
-
-
+cab = cadastroHBSIS[0::2]
+dados = cadastroHBSIS[1::2]
+print('='*30)
+print(f'{cab[0]}:{dados[0][0]} {cab[1]}: {dados[1][0]}')
+print(f'{cab[3]} de {dados[0][4]} é {dados[3][4]} anos')
+print(f'{cab[2]} de {dados[0][3]} é {dados[2][3]}')
+print('='*30)
 # 2 - usando o for, imprima todos nomes um abaixo do outro
-#
-# 3 - Usando a indexação faça uma lista com 3 bibliotecas contendo os dados do Mateus, Paulo e João
+i=-1
+for i in range(7):
+    print(f'{dados[0][i]}')
+    i = i+1
+print('='*30)
+ 
+
+# 3 - Usando a indexação faça uma lista com 3 dicionarios contendo os dados do Mateus, Paulo e João
 #  contendo como chaves: nome, email, idade, telefone (nesta  sequencia)
 
+# dic1 = {'nome':dados[0][3], 'email':dados[2][3], 'idade':dados[3][3], 'telefone':dados[1][3]}
+# dic2 = {'nome':dados[0][1], 'email':dados[2][1], 'idade':dados[3][1], 'telefone':dados[1][1]}
+# dic3 = {'nome':dados[0][5], 'email':dados[2][5], 'idade':dados[3][5], 'telefone':dados[1][5]}
+
+lista = []
+# lista.append(dic1)
+# lista.append(dic2)
+# lista.append(dic3)
+
+# print(lista)
+# print('='*30)
+
+##################
+for indice_lento in range(7):
+    dic = {}
+    for indice_rapido in range(4):
+        dic[cab[indice_rapido]] = dados[indice_rapido][indice_lento]
+    lista.append(dic)
+                                                                            
+for i in lista:
+    print(i)
