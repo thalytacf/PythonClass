53cf5bb2c71bb917204bf2bf49d64743e1f81025
Thalyta C Fischer <thalytacfischer@gmail.com>
2020-01-07 22:44:07 -0300

    update

--- Hard/29 - Aula/ex1HBA.py
+++ Hard/29 - Aula/ex1HBA.py
@@ -1,89 +1,89 @@
-print('#'*70)
-pilotos = ['piloto','chefserv','policial']
-pessoas = ['piloto','oficialA','oficialB','chefserv','comissariaA','comissariaB', 'policial','presidiario']
+# print('#'*70)
+# pilotos = ['piloto','chefserv','policial']
+# pessoas = ['piloto','oficialA','oficialB','chefserv','comissariaA','comissariaB', 'policial','presidiario']
 
-terminal = ''
-avião = []
-fortwo = []
-terminal = pessoas
-############
+# terminal = ''
+# avião = []
+# fortwo = []
+# terminal = pessoas
+# ############
 
-print(f'carrovazio {fortwo}')
-fortwo.append(pessoas[0])
-fortwo.append(pessoas[1])
-del(terminal[1])
-del(terminal[0])
-print(f'Terminal - Avião {fortwo}')
-avião.append(fortwo[1])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
-#############
-print('*'*70)
-del(fortwo[1])
-print(f'carrovazio {fortwo}')
-fortwo.append(pessoas[0])
-del(terminal[0])
-print(f'Terminal - Avião {fortwo}')
-avião.append(fortwo[1])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
-#############troca de motorista
-print('*'*70)
-fortwo.clear()
-fortwo.append(pessoas[0])
-fortwo.append(pilotos[0])
-print(f'Terminal - Avião {fortwo}')
-del(terminal[0])
-avião.append(fortwo[1])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
-##############
-print('*'*70)
-fortwo.clear()
-print(f'carrovazio {fortwo}')
-fortwo.append(pilotos[1])
-fortwo.append(pessoas[0])
-del(terminal[0])
-print(f'Terminal - Avião {fortwo}')
-avião.append(fortwo[1])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
-#############
-print('*'*70)
-del(fortwo[1])
-print(pessoas)
-print(f'carrovazio {fortwo}')
-fortwo.append(pessoas[0])
-del(terminal[0])
-print(f'Terminal - Avião {fortwo}')
-avião.append(fortwo[1])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
-#############
-print('*'*70)
-fortwo.clear()
-fortwo.append(pilotos[2])
-fortwo.append(pessoas[1])
-print(f'Terminal - Avião {fortwo}')
-del(terminal[0])
-del(terminal[0])
-terminal.append(pilotos[1])
-avião.append(fortwo[1])
-avião.append(fortwo[0])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
-############troca de motorista 2
-print('*'*70)
-fortwo.clear()
-del(avião[2])
-fortwo.append(pilotos[0])
-print(f'carrovazio {fortwo}')
-fortwo.append(terminal[0])
-del(terminal[0])
-avião.append(fortwo[1])
-avião.append(fortwo[0])
-print(f'Terminal: {terminal}')
-print(f'Avião: {avião}')
+# print(f'carrovazio {fortwo}')
+# fortwo.append(pessoas[0])
+# fortwo.append(pessoas[1])
+# del(terminal[1])
+# del(terminal[0])
+# print(f'Terminal - Avião {fortwo}')
+# avião.append(fortwo[1])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
+# #############
+# print('*'*70)
+# del(fortwo[1])
+# print(f'carrovazio {fortwo}')
+# fortwo.append(pessoas[0])
+# del(terminal[0])
+# print(f'Terminal - Avião {fortwo}')
+# avião.append(fortwo[1])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
+# #############troca de motorista
+# print('*'*70)
+# fortwo.clear()
+# fortwo.append(pessoas[0])
+# fortwo.append(pilotos[0])
+# print(f'Terminal - Avião {fortwo}')
+# del(terminal[0])
+# avião.append(fortwo[1])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
+# ##############
+# print('*'*70)
+# fortwo.clear()
+# print(f'carrovazio {fortwo}')
+# fortwo.append(pilotos[1])
+# fortwo.append(pessoas[0])
+# del(terminal[0])
+# print(f'Terminal - Avião {fortwo}')
+# avião.append(fortwo[1])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
+# #############
+# print('*'*70)
+# del(fortwo[1])
+# print(pessoas)
+# print(f'carrovazio {fortwo}')
+# fortwo.append(pessoas[0])
+# del(terminal[0])
+# print(f'Terminal - Avião {fortwo}')
+# avião.append(fortwo[1])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
+# #############
+# print('*'*70)
+# fortwo.clear()
+# fortwo.append(pilotos[2])
+# fortwo.append(pessoas[1])
+# print(f'Terminal - Avião {fortwo}')
+# del(terminal[0])
+# del(terminal[0])
+# terminal.append(pilotos[1])
+# avião.append(fortwo[1])
+# avião.append(fortwo[0])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
+# ############troca de motorista 2
+# print('*'*70)
+# fortwo.clear()
+# del(avião[2])
+# fortwo.append(pilotos[0])
+# print(f'carrovazio {fortwo}')
+# fortwo.append(terminal[0])
+# del(terminal[0])
+# avião.append(fortwo[1])
+# avião.append(fortwo[0])
+# print(f'Terminal: {terminal}')
+# print(f'Avião: {avião}')
 
 ################# METODO CARRO LIMPO
 
@@ -104,4 +104,21 @@ print(f'Avião: {avião}')
 
 #def showaviao (terminal):
 #    print(f'Terminal: {terminal}')
+
+#############################################
+# salvar terminal no arquivo
+# 
+# 
+# prinrts
+#deletes
+
+def imprimir(sair, carro, entrar):
+    print(f'Terminal: {terminal}')
+    print(f'Ida: {fortwo}')
+    print(f'Avião: {avião}')
+
+
+#MAIN CODE
+
+imprimir(terminal, fortwo, aviao)
 
--- Hard/29 - Aula/ex1clean.py
+++ Hard/29 - Aula/ex1clean.py
@@ -1,51 +1,102 @@
-###########PASSOS#########
-#--- Add passageiros no fortwo
-#--- Removo passageiros do fortwo do terminal
-#--- Imprimo o carro cheio e o terminal
-#--- Add o carona no avião
-#--- Imprimo o carro avião
-#--- TROCA DE MOTORISTA
-#---
-#---
-motoristas = ['piloto','chefserv','policial']
-passageiros = ['oficialA','oficialB','comissariaA','comissariaB','presidiario']
-aviao = []
-fortwo = []
-terminal = []
-
-################# MAIN ##############
-
-terminal.append(passageiros)
-terminal.append(motoristas)
-del(terminal[0][0])
-
-print(showterminal(terminal))
-print(f' Terminal - Avião: {addcarro(motoristas[0], passageiros[0])}')
-print(showaviao(aviao))
-
-
-
-################# METODO ENCHER CARRO
-def addcarro (pass1, pass2):
+def embarquecarro (motorista, passageiro):
     carro = []
-    carro.append(pass1)
-    carro.append(pass2)
+    carro.append(motorista)
+    carro.append(passageiro)
     return carro
 
-################# METODO SHOW PLACES
+def embarqueplat (pessoa, plataforma):
+    plataforma.append(pessoa)
+    return plataforma
 
-def showaviao (aviao):
-    print(f'Avião: {aviao}')
-
-def showterminal (terminal):
+def imprimir(sair, carro, entrar):
     print(f'Terminal: {terminal}')
+    print(f'Ida: {fortwo}')
+    print(f'Avião: {avião}')
 
-################# METODO LIMPAR
+def salvarterminal (plataforma):
+    arquivoT = open('PythonHBSIS/Hard/29 - Aula/erminal.txt','a')
+    arquivoT.append(f'{plataforma}\n')
+    arquivoT.close()
 
-def clearthis (elemento):
-    elemento.clear()
-    return elemento
+def salvaraviao (plataforma):
+    arquivoA = open('PythonHBSIS/Hard/29 - Aula/terminal.txt', 'a')
+    arquivoA.append(f'{plataforma}\n')
+    arquivoA.close()
 
-def sairterminal (terminal):
-    del(terminal[0][0])
-    return terminal
+
+
+print('#'*70)
+print(f'Ida: Terminal - Avião \nVolta: Avião - Terminal')
+print('#'*70)
+pilotos = ['piloto','chefserv','policial']
+pessoas = ['piloto','oficialA','oficialB','chefserv','comissariaA','comissariaB', 'policial','presidiario']
+
+terminal = ''
+avião = []
+fortwo = []
+terminal = pessoas
+#salvarterminal(terminal)
+############levar oficial A
+fortwo = embarquecarro(pilotos[0],pessoas[1])
+avião = embarqueplat(pessoas[1], avião)
+del(terminal[1])
+del(terminal[0])
+imprimir(terminal,fortwo, avião)
+#############levar oficial B
+print('*'*70)
+del(fortwo[1])
+print(f'Volta: {fortwo}')
+fortwo.clear()
+fortwo = embarquecarro(pilotos[0],pessoas[0])
+avião = embarqueplat(pessoas[0], avião)
+del(terminal[0])
+imprimir(terminal,fortwo, avião)
+#############troca de motorista 
+print('*'*70)
+fortwo.clear()
+fortwo = embarquecarro(pilotos[0],pessoas[0])
+avião = embarqueplat(pilotos[0], avião)
+del(terminal[0])
+imprimir(terminal,fortwo, avião)
+del(fortwo[0])
+##############levar comi A
+print('*'*70)
+print(f'Volta: {fortwo}')
+fortwo.clear()
+fortwo = embarquecarro(pilotos[1],pessoas[0])
+avião = embarqueplat(pessoas[0], avião)
+del(terminal[0])
+imprimir(terminal,fortwo, avião)
+############# levar comi B
+print('*'*70)
+del(fortwo[1])
+print(f'Volta: {fortwo}')
+fortwo.clear()
+fortwo = embarquecarro(pilotos[1],pessoas[0])
+avião = embarqueplat(pessoas[0], avião)
+del(terminal[0])
+imprimir(terminal,fortwo, avião)
+############# policia e preso
+print('*'*70)
+del(fortwo[1])
+print(f'Volta: {fortwo}')
+fortwo.clear()
+fortwo = embarquecarro(pilotos[2],pessoas[1])
+avião = embarqueplat(pessoas[1], avião)
+avião = embarqueplat(pilotos[2], avião)
+del(terminal[0])
+del(terminal[0])
+terminal.append(pilotos[1])
+imprimir(terminal,fortwo, avião)
+############troca de motorista 2 - buscar chefsev
+print('*'*70)
+fortwo.clear()
+del(avião[2])
+fortwo.append(pilotos[0])
+print(f'Volta: {fortwo}')
+fortwo = embarquecarro(pilotos[0],pilotos[1])
+del(terminal[0])
+avião = embarqueplat(pilotos[1], avião)
+avião = embarqueplat(pilotos[0], avião)
+imprimir(terminal,fortwo, avião)
+#salvaraviao(avião)
