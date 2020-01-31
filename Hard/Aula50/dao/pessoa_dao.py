e8b68b21df43d3d298effbd4b0265b4ca3be480f
Thalyta C Fischer <thalytacfischer@gmail.com>
2020-01-31 11:47:17 -0300

    'updateAPI'

---
+++ Hard/Aula50/dao/pessoa_dao.py
@@ -0,0 +1,32 @@
+from Hard.Aula50.controller.pessoa_controller import PessoaController
+import MySQLdb
+
+from Hard.Aula50.model.pessoa_model import Pessoa
+class PessoaDao:
+    def __init__(self):
+        self.connection = MySQLdb.connect(host='mysql.topskills.dev', database='topskills01', user='topskills01', passwd='ts2019')
+        self.cursor = self.connection.cursor()
+
+    def list_all(self):
+        self.cursor.execute("SELECT * FROM 01_MDG_PESSOA")
+        pessoas = self.cursor.fetchall()
+        lista_pessoa = []
+        for p in pessoas:
+            pessoa = Pessoa(p[1], p[2], p[3], p[0])
+            lista_pessoa.append(pessoa.__dict__)
+        return lista_pessoa
+
+    def get_by_id(self, id):
+        self.cursor.execute("SELECT * FROM 01_MDG_PESSOA WHERE ID = {}".format(id))
+        pessoa = self.cursor.fetchone()
+        p = Pessoa(pessoa[1], pessoa[2], pessoa[3], pessoa[0])
+        return p.__dict__
+
+    def insert(self, pessoa):
+        return 'Adicionando a pessoa : {}'.format(pessoa)
+    def update(self, pessoa):
+        return 'Alterando a pessoa : {}'.format(pessoa)
+    def remove(self, id):
+        self.cursor.execute("DELETE FROM 01_MDG_PESSOA WHERE ID = {}".format(id))
+        self.connection.commit()
+        return 'Removido a pessoa de id : {}'.format(id)
