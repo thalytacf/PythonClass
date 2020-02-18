1e7aaa8e069e3fc08108602f8b3abfab5a6adcd9
Thalyta C Fischer <thalytacfischer@gmail.com>
2020-02-18 11:52:28 -0300

    aula59

---
+++ Hard/Aula59/aula59-testes2.py
@@ -0,0 +1,28 @@
+# Teste parte 2
+
+class Calc:
+    def __init__(self, numero1, numero2):
+        # variável de classe
+        self.__n1 = numero1
+        self.__n2 = numero2
+        self.__resultado = 0
+
+    def set_n1(self, valor):
+        self.__n1 = valor
+    def get_n1(self):
+        return self.__n1
+
+    def set_n2(self, valor):
+        self.__n2 = valor
+    def get_n2(self):
+        return self.__n2
+
+    def get_resultado(self):
+        return self.__resultado
+
+    def soma(self):
+        self.__resultado = self.__n1 + self.__n2
+
+c = Calc(10,20)
+assert isinstance(c, Calc)
+assert c.soma() == c.get_resultado()
---
+++ Hard/Aula59/me.py
@@ -0,0 +1,37 @@
+
+class Calc:
+    def __init__(self, numero1, numero2):
+        # variável de classe
+        self.__n1 = numero1
+        self.__n2 = numero2
+        self.__resultado = 0
+
+    def set_n1(self, valor):
+        self.__n1 = valor
+    def get_n1(self):
+        return self.__n1
+
+    def set_n2(self, valor):
+        self.__n2 = valor
+    def get_n2(self):
+        return self.__n2
+
+    def get_resultado(self):
+        return self.__resultado
+
+    def soma(self):
+        self.__resultado = self.__n1 + self.__n2
+        return self.__resultado
+
+    def subtracao(self):
+        self.__resultado = self.__n1 - self.__n2
+        return self.__resultado
+
+    def multiplicacao(self):
+        self.__resultado = self.__n1 * self.__n2
+        return self.__resultado
+
+c =Calc(2,5)
+assert c.soma() == 7
+assert c.subtracao() == -3
+assert c.multiplicacao() == 10
